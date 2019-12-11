c4c4c967057a40f3ca4ac2a740864eca2c535d46
Itohan <itohan.ukponmwan@intel.com>
2019-12-10 16:27:06 -0800

    Refactoring, fixed metal3 import issue

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -13,7 +13,8 @@ import (
 
 	"github.com/bpa-operator/pkg/apis"
 	"github.com/bpa-operator/pkg/controller"
-        //metal3 "github.com/metal3-io/baremetal-operator/pkg/apis"
+
+         metal3 "github.com/metal3-io/baremetal-operator/pkg/apis/metal3/v1alpha1"
 	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	kubemetrics "github.com/operator-framework/operator-sdk/pkg/kube-metrics"
 	"github.com/operator-framework/operator-sdk/pkg/leader"
